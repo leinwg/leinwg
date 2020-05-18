e940d5ce709efa0b047e991c268651970c8fe29c
leinwg <63655209+leinwg@users.noreply.github.com>
2020-05-18 13:33:47 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,24 +2,35 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 
+const RpsGame = require('./rps-game');
+
 const app = express();
 
-const clientPath = '${__dirname}/../client';
-console.log('Serving static from ${clientPath}');
+const clientPath = `${__dirname}/../client`;
+console.log(`Serving static from ${clientPath}`);
 
 app.use(express.static(clientPath));
-app.use(express.static('../client'));
-app.get('/', function(req, res, next) {
-    res.send("Hello world");
-});
 
 const server = http.createServer(app);
 
 const io = socketio(server);
 
-let waitingPlayer;
+let waitingPlayer = null;
 
-io.on('connection', onConnection);
+io.on('connection', (sock) => {
+
+  if (waitingPlayer) {
+    new RpsGame(waitingPlayer, sock);
+    waitingPlayer = null;
+  } else {
+    waitingPlayer = sock;
+    waitingPlayer.emit('message', 'Waiting for an opponent');
+  }
+
+  sock.on('message', (text) => {
+    io.emit('message', text);
+  });
+});
 
 server.on('error', (err) => {
   console.error('Server error:', err);
@@ -28,18 +39,3 @@ server.on('error', (err) => {
 server.listen(8080, () => {
   console.log('RPS started on 8080');
 });
-
-function onConnection(sock) {
-    console.log('Someone connected');
-    sock.emit('message', 'Hi, you are connected');
-    sock.on('message', (text) => io.emit('message', text));
-    
-    if (waitingPlayer) {
-        // Match starts
-        io.emit('message', 'Match starts');
-        waitingPlayer = null;
-    } else {
-        waitingPlayer = sock;
-        sock.emit('message', 'You are waiting for a second player');
-    }
-};
